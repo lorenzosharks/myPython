8dd52b07b96b0ae41957e728c57c7d30d1b4fbd0
lorenzosharks <karsonx27@lakesideschool.org>
2024-10-11 11:22:52 -0700

    fixed some things

--- PolySeq.py
+++ PolySeq.py
@@ -10,14 +10,35 @@
 
 import numpy as np
 
-proceed = False
+gate1 = False    
+gate2 = False
+gate3 = False
 
-while proceed == False:
+while gate1 == False:
     try:
         try:
             poly_degree = abs(int(input("What is the degree of the polynomial: ")))
+            gate1 = True
+        except ValueError as ve:
+            print("Enter a whole number.")
+    except ValueError as ve:
+        print("Enter a whole number.")
+    
+while gate2 == False:
+    try:
+        try:
             num_terms = abs(int(input("How many terms do you want to see: ")))
+            gate2 = True
+        except ValueError as ve:
+            print("Enter a whole number.")
+    except ValueError as ve:
+        print("Enter a whole number.")
+       
+while gate3 == False: 
+    try:
+        try:
             sum_terms = abs(int(input("How many terms are added in the sum: ")))
+            gate3 = True
         except ValueError as ve:
             print("Enter a whole number.")
     except ValueError as ve:
--- test.py
+++ test.py
@@ -1,5 +1,35 @@
-import numpy as np
+gate1 = False    
+gate2 = False
+gate3 = False
 
-a = np.array([1.12476918 ,2.12863 , 3, 4])
-
-print(np.round(a))
+while gate1 == False:
+    try:
+        try:
+            poly_degree = abs(int(input("What is the degree of the polynomial: ")))
+            gate1 = True
+        except ValueError as ve:
+            print("Enter a whole number.")
+    except ValueError as ve:
+        print("Enter a whole number.")
+    
+while gate2 == False:
+    try:
+        try:
+            num_terms = abs(int(input("How many terms do you want to see: ")))
+            gate2 = True
+        except ValueError as ve:
+            print("Enter a whole number.")
+    except ValueError as ve:
+        print("Enter a whole number.")
+       
+while gate3 == False: 
+    try:
+        try:
+            sum_terms = abs(int(input("How many terms are added in the sum: ")))
+            gate3 = True
+        except ValueError as ve:
+            print("Enter a whole number.")
+    except ValueError as ve:
+        print("Enter a whole number.")
+        
+print("Done")
